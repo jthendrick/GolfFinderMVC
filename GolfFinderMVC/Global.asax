5fb7dcc6358378af2d69bcade276d51337268940
jthendrick <jthendrick93@gmail.com>
2021-05-11 11:13:07 -0400

    Add project files.

---
+++ GolfFinderMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GolfFinderMVC.MvcApplication" Language="C#" %>
---
+++ GolfFinderMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GolfFinderMVC.Startup))]
+namespace GolfFinderMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
